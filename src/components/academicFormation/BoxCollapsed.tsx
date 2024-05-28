0e76aea936a6e2cea21ccdc258cd67fb1c577aae
arthurgs2006 <arthurgonsal@hotmail.com>
2024-05-28 11:37:36 -0300

    Development of AcademicFormation Section

--- src/components/academicFormation/BoxCollapsed.tsx
+++ src/components/academicFormation/BoxCollapsed.tsx
@@ -1,25 +1,32 @@
 import { FunctionComponent, ReactNode, useState } from "react"
 import { Collapse, CardBody, Card, Button } from "reactstrap"
 
+type BoxCollapsedProps = {
+    img_src: string,
+    title: string,
+    subtitle: string,
+    children: ReactNode
+}
 
-
-export default function BoxCollapsed(props: any) {
+export default function BoxCollapsed({ img_src, title, children, subtitle }: BoxCollapsedProps) {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
     return (
         <>
-            <button className="flex-column d-flex btn btn-primary" onClick={toggle} 
-                style={{ 'width': '20vh' }} >
-                <img src={props.img_src} alt={props.title} className="img-fluid rounded cardBox" />
-                {/* <span>{description}</span> */}
-            </button>
-            <Collapse isOpen={isOpen} >
-                    <Card className="bg-secondary">
-                        <CardBody className="d-flex justify-content-center">
-                            {props.children}
+            <div className="d-flex ">
+                <a className="d-flex boxAcademicFormation" onClick={toggle}>
+                    <img src={img_src} alt={title} className="img-fluid rounded " />
+                </a>
+                <Collapse isOpen={isOpen} className="w-75 ">
+                    <Card className="bg-dark p-2">
+                        <CardBody className="d-flex justify-content-center flex-column text-white">
+                            <h1>{title}</h1>
+                            <h6>{subtitle}</h6>
+                            {children}
                         </CardBody>
                     </Card>
                 </Collapse>
+            </div>
         </>
     )
 }
--- src/pages/academicFormation/AcademicFormation.tsx
+++ src/pages/academicFormation/AcademicFormation.tsx
@@ -1,5 +1,11 @@
 import { Container } from "reactstrap"
 import BoxCollapsed from "../../components/academicFormation/BoxCollapsed"
+import SenacLogo from '../../../public/senac-logo.png'
+import IFlogo from '../../../public/if-logo.png'
+import SenaiLogo from '../../../public/senai-logo.png'
+import MicrosoftLogo from '../../../public/microsoft-logo.svg'
+import OneBitCodeLogo from '../../../public/oneBitCode-logo.png'
+
 export default function () {
     return (
         <>
@@ -11,8 +17,55 @@ export default function () {
                         </a>
                         <h1>Formações Acadêmicas</h1>
                     </div>
-                    <BoxCollapsed 
-                     />
+                    <section className="d-flex gap-3 flex-column pt-5">
+                        <BoxCollapsed img_src={SenacLogo} title={"Ensino Médio Técnico Integrado"} 
+                        subtitle="Diploma do Ensino Médio">
+                            <ul>
+                                <li>Certificado de Assistente e Suporte de Manutenção de Computadores</li>
+                                <li>Certificado de Assistente de Operação de Redes e Computadores</li>
+                                <li>Assistente de Desenvolvimento de Aplicativos Computacionais</li>
+                            </ul>
+                        </BoxCollapsed>
+                        <BoxCollapsed img_src={IFlogo} title="Análise e Desenvolvimento de Sistemas" 
+                        subtitle="Graduação">
+                            <ul>
+                                <li>Desenvolvimento Web (Javascript, CSS e HTML)</li>
+                                <li>Desenvolvimento com C</li>
+                                <li>Cálculo</li>
+                                <li>Lógica de Programação</li>
+                            </ul>
+                        </BoxCollapsed>
+                        <BoxCollapsed img_src={SenaiLogo} title="Power BI" 
+                        subtitle="Curso Profissionalizante">
+                            <ul>
+                                <li>Criação de dashboards interativas e dinâmicas</li>
+                                <li>Análise e Alteração de dados</li>
+                                <li>Organização de dados e processos analíticos</li>
+                            </ul>
+                        </BoxCollapsed>
+                        <BoxCollapsed img_src={MicrosoftLogo} title="Fundamentos do Azure"
+                        subtitle="Curso Profissionalizante">
+                            <ul>
+                                <li>SC-900: Microsoft Certified: Security, Compliance and Identity Fundamentals</li>
+                                <li>AZ-900: Microsoft Certified: Azure Data Fundamentals</li>
+                            </ul>
+                        </BoxCollapsed>
+                        <BoxCollapsed img_src={OneBitCodeLogo} title="Javascript, frameworks e ferramentas"
+                        subtitle="Curso Profissionalizante">
+                            <ul>
+                                <li>Certificado JavaScript</li>
+                                <li>Certificado HTML5</li>
+                                <li>Certificado CSS3</li>
+                                <li>Certificado de Javascript (Lógica de Programação)</li>
+                                <li>Certificado de database relacional (PostgreSQL)</li>
+                                <li>Certificado de Typescript</li>
+                                <li>Certificado de SASS</li>
+                                <li>Certificado de NextJS</li>
+                                <li>Certificado de Bootstrap</li>
+                                <li>Certificado de Fundamentos do React</li>
+                            </ul>
+                        </BoxCollapsed>
+                    </section>
                 </Container>
             </div>
         </>
