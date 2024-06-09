b7eeb66da6751dfcc601709d1515b17b7321dc1a
arthurgs2006 <arthurgonsal@hotmail.com>
2024-06-08 21:30:36 -0300

    Development of Contact Section v1.3

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -25,10 +25,15 @@ export default function MainContent() {
                         </Row>
                     </section>
                     <Row className="flex-row gap-4 px-2">
-                        <Box icon={'collection-fill'} description={'Meus projetos'} href="/myProjects" target={false} />
-                        <Box icon={'person-workspace'} description={"Histórico Profissional"} href="professional_experiences" target={false} />
-                        <Box icon={"linkedin"} description={"Meu LinkedIn"} href="" target={true} />
-                        <Box icon={"file-earmark-richtext-fill"} description={'Meu Currículo'} href="" target={true} />
+                        <Box icon={'collection-fill'} description={'Meus projetos'} 
+                        href="/myProjects" target={false} />
+                        <Box icon={'person-workspace'} description={"Histórico Profissional"} 
+                        href="professional_experiences" target={false} />
+                        <Box icon={"linkedin"} description={"Meu LinkedIn"} 
+                        href="https://www.linkedin.com/in/arthur-gonçalves-saldanha-375a70157" target={true} />
+                        <Box icon={"file-earmark-richtext-fill"} description={'Meu Currículo'} 
+                        href="https://drive.google.com/file/d/1FYl3iC4gEwcw1MVYhEyIQW1m18sLAuml/view?usp=sharing" 
+                        target={true} />
                     </Row>
                 </Container>
             </section>
--- src/components/contactsOffCanva/Contacts.tsx
+++ src/components/contactsOffCanva/Contacts.tsx
@@ -23,10 +23,81 @@ export default function Contact() {
                     <i className="bi bi-upload"></i>
                     <i className="bi bi-person-circle"></i>
                 </header>
+                <hr />
                 {/* </OffcanvasHeader> */}
-                <OffcanvasBody className="px-4 pt-4">
+                <OffcanvasBody className="px-4 pt-0">
+                    <header className="title">
                         <h3>Contatos:</h3>
-                        
+                        <h5>Quer enviar uma mensagem?</h5>
+                    </header>
+                    <form action="mailto:arthurgonsal@hotmail.com"
+                        method="POST" encType="text/plain"
+                        className="d-flex gap-3 flex-column">
+                        <div className="name form-group d-flex flex-column gap-1 pt-2">
+                            <label htmlFor="name" className="">
+                                Nome:
+                            </label>
+                            <input type="text" id="name" name="name" maxLength={60}
+                                className="input form-control"
+                                placeholder="Insira seu nome..."
+                                required />
+                        </div>
+                        <div className="email form-group d-flex flex-column gap-1">
+                            <label htmlFor="email" className="">
+                                E-mail:
+                            </label>
+                            <input type="email" id="email" name="e-mail"
+                                className="input form-control"
+                                placeholder="Insira seu e-mail..."
+                                required />
+                        </div>
+                        <div className="message form-group d-flex flex-column gap-1">
+                            <label htmlFor="message" className="">
+                                Mensagem:
+                            </label>
+                            <textarea id="message"
+                                placeholder="Escreva sua mensagem..."
+                                className="input form-control" rows={5} name="message"
+                                required />
+                        </div>
+                        <div className="btn-area d-flex gap-3">
+                            <input type="submit" value="Enviar" className="btn btn-success w-50" />
+                            <input type="reset" value="Limpar" className="btn btn-success w-50" />
+                        </div>
+                    </form>
+                    <hr />
+                    <footer className="data-info">
+                        <h5>Informações de Contato:</h5>
+                        <h6>Números para Contato:</h6>
+                        <ul>
+                            <li>(16) 99242-8919</li>
+                            <li>(11) 99735-4513</li>
+                        </ul>
+                        <h6 className="pt-3">Redes Sociais:</h6>
+                        <div className="d-flex h1 gap-3 justify-content-between pt-2">
+                            <a href="https://www.linkedin.com/in/arthur-gonçalves-saldanha-375a70157"
+                                target="_blank">
+                                <i className="bi bi-linkedin text-success"></i>
+                            </a>
+                            <a href="https://api.whatsapp.com/send?phone=11997354513"
+                                target="_blank">
+                                <i className="bi bi-whatsapp text-success"></i>
+                            </a>
+                            <a href="https://github.com/arthurgs2006"
+                                target="_blank">
+                                <i className="bi bi-github text-success"></i>
+                            </a>
+                            <a href="https://open.spotify.com/user/arthurgonsal123?si=bMwUo-3NQZ-XZFnVU10Agg"
+                                target="_blank">
+                                <i className="bi bi-spotify text-success"></i>
+                            </a>
+                            <a href="mailto:arthurgonsal@hotmail.com"
+                                target="_blank">
+                                <i className="bi bi-envelope-at-fill text-success"></i>
+                            </a>
+                        </div>
+                    </footer>
+
                 </OffcanvasBody>
             </Offcanvas>
         </>
